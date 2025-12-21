a3f1bb3cc97ae449410593925ef67dc0dc16af41
songs0217 <songs0217@gmail.com>
2025-12-21 16:26:27 +0000

    오류 수정

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -1,30 +1,25 @@
 import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
-// This function creates a Supabase client that can be used in Server Components,
-// Server Actions, and Route Handlers.
 export const createClient = () => {
-  const cookieStore = cookies()
-
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        get(name: string) {
+        get: async (name: string) => {
+          const cookieStore = await cookies()
           return cookieStore.get(name)?.value
         },
-        set(name: string, value: string, options: CookieOptions) {
-          // In a Server Action, calling `cookieStore.set` will set a response header.
-          // This will be sent back to the browser on form submission or redirect.
+        set: async (name: string, value: string, options: CookieOptions) => {
+          const cookieStore = await cookies()
           cookieStore.set({ name, value, ...options })
         },
-        remove(name: string, options: CookieOptions) {
-          // In a Server Action, calling `cookieStore.set` with an empty value
-          // and an expired date will delete the cookie.
+        remove: async (name: string, options: CookieOptions) => {
+          const cookieStore = await cookies()
           cookieStore.set({ name, value: '', ...options })
         },
       },
-    },
+    }
   )
 }
